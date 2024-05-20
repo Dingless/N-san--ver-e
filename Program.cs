ae4dea3ab607c6f30e962024485defb6be33ef8e
Dingless <mohameddeg.abdulahi@elev.ga.ntig.se>
2024-05-20 13:38:00 +0200

    skapat tre instanser och lagt till publika variablen current page met metoden  turn page

--- Program.cs
+++ Program.cs
@@ -1,6 +1,15 @@
 ﻿public class Book
 {
-    public string name{get;set;}
+    //De tre publika variablerna
+    public string Name{get;set;}
     public int pages {get;set;}
-
+    public int currentpage {get;set;} = 0;
+    public void turnpage(){
+        currentpage++;
+    }
 }
+// tre instanser av variabeln 
+    Book book1 = new Book {Name = "The Great Gatsby", pages = 180};
+    Book book2 = new Book {Name = "To kill a mockingbird", pages = 281};
+    Book book3 = new Book {Name = "1984", pages = 328};
+    
